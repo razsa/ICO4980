ec60c77d9be03720cf51861153a621e35f36887e
Razsa <21493e@gmail.com>
2020-03-14 00:28:39 -0400

    3

--- contracts/KToken.sol
+++ contracts/KToken.sol
@@ -2,11 +2,12 @@ pragma solidity ^0.5.11;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./KTokenCrowdsale.sol";
 
 contract KToken is ERC20, ERC20Detailed {
     constructor(string memory _name, string memory _symbol, uint8 _decimals)
 	ERC20Detailed(_name, _symbol, _decimals)
 	public {
-}
+	}
 }
 
